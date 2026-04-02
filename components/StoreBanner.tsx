b033f18ea340bb546f196a14901b4fc3857fff68
Fletcher15478 <fletchhartsock93@gmail.com>
2026-04-02 13:26:32 -0400

    fix banner

--- components/StoreBanner.tsx
+++ components/StoreBanner.tsx
@@ -9,7 +9,8 @@ export function StoreBanner() {
     >
       <div className="store-banner-inner">
         <p className="store-banner-line">ALL ITEMS ARE IN STOCK!</p>
-        <p className="store-banner-line">WE SHIP EVERY WEDNESDAY</p>
+        <span className="store-banner-divider" aria-hidden />
+        <p className="store-banner-line store-banner-line-secondary">WE SHIP EVERY WEDNESDAY</p>
       </div>
     </div>
   );
